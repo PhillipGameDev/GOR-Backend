803163dab30e36b5d76ef83a8f883396a2c81afc
PhillipGameDev <267723+PhillipGameDev@users.noreply.github.com>
2022-06-22 23:48:56 +0900

    Added test solution

---
+++ GameOfRevenge.WebServerTest/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using GameOfRevenge.Business;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace GameOfRevenge.WebServer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //            Config.ConnectionString = ConfigurationManager.AppSettings["ConString"];
+            Config.ConnectionString = "Data Source=52.194.60.233,1433;Initial Catalog=GameOfRevenge;Persist Security Info=True;User ID=developer;Password=developer";
+
+            Config.DefaultWorldCode = ConfigurationManager.AppSettings["DefaultWorldCode"];
+
+            Console.WriteLine(">>>>>> Main "+ Config.ConnectionString);
+            Test().Wait();
+        }
+
+        private static async Task Test()
+        {
+            await Startup.ReloadDataBaseDataAsync();
+        }
+    }
+}
