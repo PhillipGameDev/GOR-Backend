ac6779e05e6425be0ef4fd847cc58ec7134a2e50
PhillipGameDev <267723+PhillipGameDev@users.noreply.github.com>
2022-08-29 17:53:08 +0900

    Changes for Store Procedures

---
+++ StoreProcedures/Table_HeroDataRel.sql
@@ -0,0 +1,128 @@
+DECLARE @HEROID INT = 0;
+
+--Points		1
+--Power			2
+--AttackCount	3
+--AttackFail	4
+--DefenseCount	5
+--DefenseFail	6
+
+SET @HEROID = 1;--LordOfTheSeas
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 1);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 2);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 3);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 4);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 5);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 6);
+
+SET @HEROID = 2;--TheVampireSultan
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 1);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 2);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 3);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 4);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 5);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 6);
+
+SET @HEROID = 3;--FamousWarrior
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 1);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 2);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 3);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 4);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 5);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 6);
+
+SET @HEROID = 4;--LightOfSultan
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 1);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 2);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 3);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 4);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 5);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 6);
+
+SET @HEROID = 5;--TribalKnight
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 1);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 2);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 3);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 4);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 5);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 6);
+
+SET @HEROID = 6;--TigerHeart
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 1);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 2);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 3);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 4);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 5);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 6);
+
+SET @HEROID = 7;--DesertKnight
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 1);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 2);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 3);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 4);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 5);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 6);
+
+SET @HEROID = 8;--JusticeKnight
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 1);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 2);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 3);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 4);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 5);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 6);
+
+SET @HEROID = 9;--BeautyKnight
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 1);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 2);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 3);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 4);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 5);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 6);
+
+SET @HEROID = 10;--AlyamamahEyes
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 1);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 2);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 3);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 4);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 5);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 6);
+
+SET @HEROID = 11;
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 1);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 2);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 3);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 4);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 5);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 6);
+
+SET @HEROID = 12;
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 1);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 2);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 3);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 4);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 5);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 6);
+
+SET @HEROID = 13;
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 1);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 2);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 3);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 4);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 5);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 6);
+
+SET @HEROID = 14;
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 1);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 2);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 3);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 4);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 5);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 6);
+
+SET @HEROID = 15;
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 1);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 2);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 3);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 4);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 5);
+INSERT INTO [dbo].[HeroDataRel] VALUES (@HEROID, 6);
