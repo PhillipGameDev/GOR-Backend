4eb25fecba1578d7ba7ef8e23c4671cb4bfbc8ba
PhillipGameDev <267723+PhillipGameDev@users.noreply.github.com>
2023-12-03 00:00:12 +0900

    Updated stored procedure

--- StoredProcedures/GetAllMail.sql
+++ StoredProcedures/GetAllMail.sql
@@ -1,6 +1,6 @@
 USE [GameOfRevenge]
 GO
-/****** Object:  StoredProcedure [dbo].[GetAllMail]    Script Date: 6/1/2023 2:37:57 AM ******/
+/****** Object:  StoredProcedure [dbo].[GetAllMail]    Script Date: 12/1/2023 4:08:46 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -8,15 +8,18 @@ GO
 
 
 ALTER   PROCEDURE [dbo].[GetAllMail]
-	@PlayerId INT
+	@PlayerId INT,
+	@LastTime DATETIME = NULL
 AS
 BEGIN
 	DECLARE @case INT = 1, @error INT = 0;
 	DECLARE @message NVARCHAR(MAX) = NULL;
 	DECLARE @time DATETIME = CURRENT_TIMESTAMP;
+	DECLARE @timeAgo DATETIME = NULL;
 	DECLARE @userId INT = @PlayerId;
 	DECLARE @cId INT = NULL;
 	SELECT @cId = [PlayerId] FROM [dbo].[Player] WHERE [PlayerId] = @userId;
+	SELECT @timeAgo = DATEADD(DAY, -10, @time);
 	
 	IF (@cId IS NULL)
 		BEGIN
@@ -33,7 +36,7 @@ BEGIN
 	SELECT e.[MailId], CAST(e.[MailContentTypeId] AS TINYINT) AS 'MailType', e.[Content], e.[IsRead], e.[IsSaved], e.[CreateDate]
 	FROM [dbo].[Mail] AS e
 /*	INNER JOIN [dbo].[MailContentType] AS c ON c.[MailContentTypeId] = e.[MailContentTypeId]*/
-	WHERE e.[PlayerId] = @cId;
+	WHERE e.[PlayerId] = @cId AND (@LastTime IS NULL OR e.[CreateDate] > @LastTime) AND (e.[IsSaved] = 1 OR e.[CreateDate] >= @timeAgo);
 
 	EXEC [dbo].[GetMessage] @userId, @message, @case, @error, @time, 1, 1;
 END
